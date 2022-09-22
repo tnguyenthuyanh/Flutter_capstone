cb47aed110ea9d9e6ff415720b087dd354921963
Landon <landonmanuel34@yahoo.com>
2022-09-22 00:02:56 -0500

    forgotSignIn screen init

---
+++ lib/viewscreen/ForgotSignIn_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ForgotSignIn extends StatefulWidget {
+  const ForgotSignIn({Key? key}) : super(key: key);
+
+  @override
+  State<StatefulWidget> createState() => _ForgotName();
+}
+
+class _ForgotName extends State<ForgotSignIn> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
