dee0df3efbc17ed94c181af57212a391c2b002af
AnTruong <truonganbest2010@gmail.com>
2022-09-24 00:35:56 -0500

    add stars for rating

--- lib/viewscreen/tools_screen/tipcalculator_screen.dart
+++ lib/viewscreen/tools_screen/tipcalculator_screen.dart
@@ -1,9 +1,12 @@
+import 'package:cap_project/viewscreen/tools_screen/view/star_rating.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class TipCalculatorScreen extends StatefulWidget {
   static const routeName = '/tipCalculatorScreen';
 
-  const TipCalculatorScreen({Key? key}) : super(key: key);
+  const TipCalculatorScreen({required this.user, Key? key}) : super(key: key);
+  final User user;
 
   @override
   State<StatefulWidget> createState() {
@@ -12,16 +15,54 @@ class TipCalculatorScreen extends StatefulWidget {
 }
 
 class _TipCalculatorState extends State<TipCalculatorScreen> {
+  late _Controller con;
+  double rating = 0;
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+    con = _Controller(this);
+  }
+
   @override
   Widget build(BuildContext context) {
+    final Object? args = ModalRoute.of(context)!.settings.arguments;
     return WillPopScope(
       onWillPop: () => Future.value(true),
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Tip Calculator"),
-        ),
-        body: const Text("This is tip calc screen"),
-      ),
+          appBar: AppBar(
+            title: const Text("Tip Calculator"),
+          ),
+          body: GestureDetector(
+              onTap: () {
+                FocusScope.of(context).unfocus();
+              },
+              child: Form(
+                key: formKey,
+                child: SingleChildScrollView(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(hintText: "Amount"),
+                        keyboardType: TextInputType.number,
+                        maxLines: 1,
+                      ),
+                      StarRating(
+                        rating: rating,
+                        onRatingChanged: (rating) => setState(() => this.rating = rating),
+                        color: Colors.cyan,
+                      )
+                    ],
+                  ),
+                ),
+              ))),
     );
   }
+}
+
+class _Controller {
+  _TipCalculatorState state;
+  _Controller(this.state);
 }
