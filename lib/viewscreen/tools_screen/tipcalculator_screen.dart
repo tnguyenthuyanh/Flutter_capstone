4037354c30f201c041d3938072dc2ded41b96861
AnTruong <truonganbest2010@gmail.com>
2022-09-28 13:55:26 -0500

    added dialog for showing result

--- lib/viewscreen/tools_screen/tipcalculator_screen.dart
+++ lib/viewscreen/tools_screen/tipcalculator_screen.dart
@@ -51,7 +51,7 @@ class _TipCalculatorState extends State<TipCalculatorScreen> {
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
                   Container(
-                    color: Colors.green.shade900,
+                    color: Colors.grey.shade600,
                     padding: const EdgeInsets.fromLTRB(30, 20, 30, 20),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
@@ -66,6 +66,7 @@ class _TipCalculatorState extends State<TipCalculatorScreen> {
                           validator: (value) {
                             return TipCalc.validatePurchaseAmount(value);
                           },
+                          onChanged: (value) => {formKey.currentState!.validate()},
                           onSaved: (value) {
                             con.savePurchaseAmount(value);
                           },
@@ -96,7 +97,7 @@ class _TipCalculatorState extends State<TipCalculatorScreen> {
                                   Card(
                                     color: Colors.red.shade900,
                                     child: InkWell(
-                                      splashColor: Colors.red.shade500.withAlpha(50),
+                                      splashColor: Colors.red.shade500.withAlpha(90),
                                       onTap: () => setState(() {
                                         con.star = 0;
                                       }),
@@ -227,6 +228,7 @@ class _Controller {
     tipPerPerson = totalTip / numOfPeople;
     amountPerPerson = totalPay / numOfPeople;
 
+    tipcalc.timestamp = DateTime.now();
     tipcalc.purchaseAmount = purchaseAmount;
     tipcalc.star = star;
     tipcalc.numOfPeople = numOfPeople;
@@ -241,6 +243,7 @@ class _Controller {
   void showCustomDialog(BuildContext context, TipCalc tc) {
     showGeneralDialog(
       context: context,
+      useRootNavigator: false,
       barrierLabel: "Barrier",
       barrierDismissible: true,
       barrierColor: Colors.black.withOpacity(0.5),
@@ -251,32 +254,180 @@ class _Controller {
             height: 300,
             margin: const EdgeInsets.symmetric(horizontal: 20),
             decoration: BoxDecoration(
-              color: Colors.green.shade100,
-              borderRadius: BorderRadius.circular(40),
+              color: Colors.black.withAlpha(100),
+              borderRadius: BorderRadius.circular(30),
             ),
             child: Padding(
               padding: const EdgeInsets.all(10),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Text(
-                    "Total Pay: \$" + tc.totalPay!.toStringAsFixed(2).toString(),
-                    style: const TextStyle(fontSize: 14, color: Colors.black),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade600,
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(20.0)),
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Total Pay: ",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black.withAlpha(50),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            "\$ " + tc.totalPay!.toStringAsFixed(2).toString(),
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                    "Total Tip: \$" + tc.totalTip!.toStringAsFixed(2).toString(),
-                    style: const TextStyle(fontSize: 14, color: Colors.black),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade700,
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text(
+                              "Total Tip: ",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black.withAlpha(50),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: EdgeInsets.all(5),
+                              child: Text(
+                                "\$ " + tc.totalTip!.toStringAsFixed(2).toString(),
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  decoration: TextDecoration.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              "Tip Per Person: ",
+                              style: const TextStyle(
+                                fontSize: 14,
+                                color: Colors.white,
+                                decoration: TextDecoration.none,
+                              ),
+                            ),
+                            Container(
+                              decoration: BoxDecoration(
+                                color: Colors.black.withAlpha(50),
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              padding: EdgeInsets.all(5),
+                              child: Text(
+                                "\$ " + tc.tipPerPerson!.toStringAsFixed(2).toString(),
+                                style: const TextStyle(
+                                  fontSize: 14,
+                                  color: Colors.white,
+                                  decoration: TextDecoration.none,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
-                  Text(
-                    "Tip Per Person: \$" + tc.tipPerPerson!.toStringAsFixed(2).toString(),
-                    style: const TextStyle(fontSize: 14, color: Colors.black),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.green.shade900,
+                      // borderRadius: BorderRadius.vertical(bottom: Radius.circular(20.0)),
+                    ),
+                    padding: const EdgeInsets.fromLTRB(10, 30, 10, 30),
+                    child: Row(
+                      children: [
+                        Text(
+                          "Amount Per Person: ",
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.white,
+                            decoration: TextDecoration.none,
+                          ),
+                        ),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.black.withAlpha(50),
+                            borderRadius: BorderRadius.circular(20),
+                          ),
+                          padding: EdgeInsets.all(5),
+                          child: Text(
+                            "\$ " + tc.amountPerPerson!.toStringAsFixed(2).toString(),
+                            style: const TextStyle(
+                              fontSize: 14,
+                              color: Colors.white,
+                              decoration: TextDecoration.none,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
-                    child: Text(
-                      "Amount Per Person: \$" +
-                          tc.amountPerPerson!.toStringAsFixed(2).toString(),
-                      style: const TextStyle(fontSize: 14, color: Colors.black),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(MediaQuery.of(context).size.width * 0.3,
+                        20, MediaQuery.of(context).size.width * 0.3, 20),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.vertical(bottom: Radius.circular(20.0)),
+                      color: Colors.black,
+                    ),
+                    child: Card(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(40.0),
+                      ),
+                      color: Colors.cyan.shade900,
+                      child: InkWell(
+                        splashColor: Colors.red.shade500.withAlpha(50),
+                        onTap: () {
+                          state.setState(() {
+                            numOfPeople = 1;
+                            star = 0;
+                          });
+                          print("Saved!");
+                          state.formKey.currentState!.reset();
+                          Navigator.pop(context, true);
+                        },
+                        child: SizedBox(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: const [
+                              Text("Save "),
+                              SizedBox(width: 10),
+                              Icon(Icons.cloud_upload),
+                            ],
+                          ),
+                        ),
+                      ),
                     ),
                   ),
                 ],
