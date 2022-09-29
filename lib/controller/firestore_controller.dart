63d630a720065133efc5d3a8faf8acc42dbdaf2e
AnTruong <truonganbest2010@gmail.com>
2022-09-29 13:34:12 -0500

    add get tip calc list

--- lib/controller/firestore_controller.dart
+++ lib/controller/firestore_controller.dart
@@ -15,10 +15,26 @@ class FirestoreController {
     return ref.id; // doc id auto-generated.
   }
 
+  // tools - save tip calc
   static Future<String> saveTipCalc(TipCalc tc) async {
     var ref = await FirebaseFirestore.instance
         .collection(Constant.savedTipCalc)
         .add(tc.toFirestoreDoc());
     return ref.id;
+  }
+
+  // tools - get tipcalc list
+  static Future<List<TipCalc>> getTipCalcList({required String email}) async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection(Constant.savedTipCalc)
+        .where(TipCalc.CREATE_BY, isEqualTo: email)
+        .orderBy(TipCalc.TIMESTAMP, descending: true)
+        .get();
+    var result = <TipCalc>[];
+    querySnapshot.docs.forEach((m) {
+      var data = m.data() as Map<String, dynamic>;
+      result.add(TipCalc.fromFirestoreDoc(docId: m.id, doc: data));
+    });
+    return result;
   }
 }
