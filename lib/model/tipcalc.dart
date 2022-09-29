c76ab99c285e49697b7b3a397a6e5d7bbcd2d3a9
AnTruong <truonganbest2010@gmail.com>
2022-09-29 13:33:43 -0500

    added constant and remove enum

--- lib/model/tipcalc.dart
+++ lib/model/tipcalc.dart
@@ -1,15 +1,4 @@
-enum DocKeyCalcTip {
-  createdBy,
-  timestamp,
-  purchaseAmount,
-  star,
-  numOfPeople,
-  note,
-  totalTip,
-  totalPay,
-  tipPerPerson,
-  amountPerPerson,
-}
+// ignore_for_file: constant_identifier_names
 
 class TipCalc {
   String? docId;
@@ -38,40 +27,50 @@ class TipCalc {
     this.amountPerPerson,
   }) {}
 
+  static const CREATE_BY = 'createdBy';
+  static const TIMESTAMP = 'timestamp';
+  static const PURCHASE_AMOUNT = 'purchaseAmount';
+  static const STAR = 'star';
+  static const NUM_OF_PEOPLE = 'numOfPeople';
+  static const NOTE = 'note';
+  static const TOTAL_TIP = 'totalTip';
+  static const TOTAL_PAY = 'totalPay';
+  static const TIP_PER_PERSON = 'tipPerPerson';
+  static const AMOUNT_PER_PERSON = 'amountPerPerson';
+
   // serialization
   Map<String, dynamic> toFirestoreDoc() {
     return {
-      DocKeyCalcTip.createdBy.name: createBy,
-      DocKeyCalcTip.timestamp.name: timestamp,
-      DocKeyCalcTip.purchaseAmount.name: purchaseAmount,
-      DocKeyCalcTip.star.name: star,
-      DocKeyCalcTip.numOfPeople.name: numOfPeople,
-      DocKeyCalcTip.note.name: note,
-      DocKeyCalcTip.totalTip.name: totalTip,
-      DocKeyCalcTip.totalPay.name: totalPay,
-      DocKeyCalcTip.tipPerPerson.name: tipPerPerson,
-      DocKeyCalcTip.amountPerPerson.name: amountPerPerson,
+      CREATE_BY: createBy,
+      TIMESTAMP: timestamp,
+      PURCHASE_AMOUNT: purchaseAmount,
+      STAR: star,
+      NUM_OF_PEOPLE: numOfPeople,
+      NOTE: note,
+      TOTAL_TIP: totalTip,
+      TOTAL_PAY: totalPay,
+      TIP_PER_PERSON: tipPerPerson,
+      AMOUNT_PER_PERSON: amountPerPerson,
     };
   }
 
   // deserialization
-  static TipCalc? fromFirestoreDoc(
+  static TipCalc fromFirestoreDoc(
       {required String docId, required Map<String, dynamic> doc}) {
     return TipCalc(
       docId: docId,
-      createBy: doc[DocKeyCalcTip.createdBy.name],
-      timestamp: doc[DocKeyCalcTip.timestamp.name] == null
+      createBy: doc[CREATE_BY],
+      timestamp: doc[TIMESTAMP] == null
           ? null
-          : DateTime.fromMillisecondsSinceEpoch(
-              doc[DocKeyCalcTip.timestamp].millisecondsSinceEpoch),
-      purchaseAmount: doc[DocKeyCalcTip.purchaseAmount.name],
-      star: doc[DocKeyCalcTip.star.name],
-      numOfPeople: doc[DocKeyCalcTip.numOfPeople.name],
-      note: doc[DocKeyCalcTip.note.name],
-      totalTip: doc[DocKeyCalcTip.totalTip.name],
-      totalPay: doc[DocKeyCalcTip.totalPay.name],
-      tipPerPerson: doc[DocKeyCalcTip.tipPerPerson.name],
-      amountPerPerson: doc[DocKeyCalcTip.amountPerPerson.name],
+          : DateTime.fromMillisecondsSinceEpoch(doc[TIMESTAMP].millisecondsSinceEpoch),
+      purchaseAmount: doc[PURCHASE_AMOUNT],
+      star: doc[STAR],
+      numOfPeople: doc[NUM_OF_PEOPLE],
+      note: doc[NOTE],
+      totalTip: doc[TOTAL_TIP],
+      totalPay: doc[TOTAL_PAY],
+      tipPerPerson: doc[TIP_PER_PERSON],
+      amountPerPerson: doc[AMOUNT_PER_PERSON],
     );
   }
 
