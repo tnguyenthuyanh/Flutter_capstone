94882522f5708ee9a3c6b298e3305a895f4b56cb
brotato-cmd <martinez.nick.r@gmail.com>
2022-09-29 09:48:35 -0500

    updates

--- lib/controller/auth_controller.dart
+++ lib/controller/auth_controller.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
-
 import '../model/constant.dart';
 import '../model/user.dart';
 import '../viewscreen/userhome_screen.dart';
