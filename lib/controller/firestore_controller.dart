7a66f6a0555f69849b0aaaa9f8db494d2da1777a
Justin Waldeck <tha1geye@gmail.com>
2022-10-01 15:59:34 -0500

    stashing issues with store controller

--- lib/controller/firestore_controller.dart
+++ lib/controller/firestore_controller.dart
@@ -65,26 +65,26 @@ class FirestoreController {
     return ref.id; // doc id auto-generated.
   }
 
-  static Future<List<Debt>> getDebtList({
-    required UserProfile user,
-  }) async {
-    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-        .collection(Constant.users)
-        .doc(user.docId)
-        .collection(Constant.debts)
-        .orderBy(DocKeyDebt.title.name, descending: true)
-        .get();
+  // static Future<List<Debt>> getDebtList({
+  //   required UserProfile user,
+  // }) async {
+  //   QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+  //       .collection(Constant.users)
+  //       .doc(user.docId)
+  //       .collection(Constant.debts)
+  //       .orderBy(DocKeyDebt.title.name, descending: true)
+  //       .get();
 
-    var result = <Debt>[];
-    for (var doc in querySnapshot.docs) {
-      if (doc.data() != null) {
-        var document = doc.data() as Map<String, dynamic>;
-        var p = Debt.fromFirestoreDoc(doc: document, docId: doc.id);
-        if (p != null) result.add(p);
-      }
-    }
-    return result;
-  }
+  //   var result = <Debt>[];
+  //   for (var doc in querySnapshot.docs) {
+  //     if (doc.data() != null) {
+  //       var document = doc.data() as Map<String, dynamic>;
+  //       var p = Debt.fromFirestoreDoc(doc: document, docId: doc.id);
+  //       if (p != null) result.add(p);
+  //     }
+  //   }
+  //   return result;
+  // }
 
   static Future<UserProfile> getUser({
     required String email,
