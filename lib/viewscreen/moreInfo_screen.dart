3f4b03a40b4ee61ed7f98d821cd1b9762f97a93b
brotato-cmd <martinez.nick.r@gmail.com>
2022-10-28 17:13:41 -0500

    quick upd

--- lib/viewscreen/moreInfo_screen.dart
+++ lib/viewscreen/moreInfo_screen.dart
@@ -63,15 +63,17 @@ class _MoreInfoState extends State<MoreInfoScreen> {
                       ],
                     )),
                     Container(
-                        width: 100,
-                        height: 100,
-                        decoration: BoxDecoration(
-                            color: Colors.grey,
-                            borderRadius: BorderRadius.circular(8.0),
-                            image: DecorationImage(
-                              fit: BoxFit.fill,
-                              image: NetworkImage(_item.img),
-                            ))),
+                      width: 100,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        color: Colors.grey,
+                        borderRadius: BorderRadius.circular(8.0),
+                        image: DecorationImage(
+                          fit: BoxFit.fill,
+                          image: NetworkImage(_item.img),
+                        ),
+                      ),
+                    ),
                   ],
                 ),
               );
